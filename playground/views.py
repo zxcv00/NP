00d5f7a06d96a0c2481793eb90220a1815c064a0
zxcv00 <w2012@e-mirim.hs.kr>
2022-03-21 14:50:37 +0900

    views: {'name': '이름'} -> templates: {{ name }}

--- playground/views.py
+++ playground/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def say_hello(request):
+    return HttpResponse("Hello, World!")
+
+def say_hello_html(request):
+    return render(request, 'playground/hello.html', {'name': 'sksk'})
+
 def say_bye(request):
-    return HttpResponse("Good Bye!")
-
-
-def say_bye_html(request):
     return render(request, 'playground/bye.html')
