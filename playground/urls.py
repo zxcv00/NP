65f704d51810261508f3eda5151ab6585d2f6527
zxcv00 <w2012@e-mirim.hs.kr>
2022-03-21 14:41:00 +0900

    playground/ -> bye/ -> say_bye() -> bye.html

--- playground/urls.py
+++ playground/urls.py
@@ -4,6 +4,6 @@ from playground import views
 app_name = 'playground'
 
 urlpatterns = [
-    path('hello/', views.say_hello, name='hello'),  # playground: hello
-    path('hello_html/', views.say_hello_html, name='hello_html'),
+    path('bye/', views.say_bye, name='bye'),  # playground: hello
+    path('bye_html/', views.say_bye_html, name='bye_html'),
 ]
--- playground/views.py
+++ playground/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def say_hello(request):
-    return HttpResponse("Hello, World!")
+def say_bye(request):
+    return HttpResponse("Good Bye!")
 
 
-def say_hello_html(request):
-    return render(request, 'playground/hello.html')
+def say_bye_html(request):
+    return render(request, 'playground/bye.html')
