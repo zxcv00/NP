ea89a34c3725d11cea7be9a5e06e190a9b404cc6
zxcv00 <w2012@e-mirim.hs.kr>
2022-03-21 14:24:59 +0900

    playground/urls: hello_html

--- playground/urls.py
+++ playground/urls.py
@@ -5,4 +5,5 @@ app_name = 'playground'
 
 urlpatterns = [
     path('hello/', views.say_hello, name='hello'),  # playground: hello
+    path('hello_html/', views.say_hello_html, name='hello_html'),
 ]
